8850ac14aa00ea18f029ccabd0d6a8d4d2b555c3
Festus Agboma <festusyuma@gmail.com>
2020-07-21 13:40:17 +0100

    call customer

--- app/src/main/java/festusyuma/com/glaiddriver/utilities/NewOrderFragment.kt
+++ app/src/main/java/festusyuma/com/glaiddriver/utilities/NewOrderFragment.kt
@@ -3,19 +3,14 @@ package festusyuma.com.glaiddriver.utilities
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import festusyuma.com.glaiddriver.R
 import festusyuma.com.glaiddriver.controller.ChatActivity
-import festusyuma.com.glaiddriver.helpers.API_LOG_TAG
 import festusyuma.com.glaiddriver.helpers.addCountryCode
-import festusyuma.com.glaiddriver.helpers.buttonClickAnim
 import festusyuma.com.glaiddriver.helpers.capitalizeWords
 import festusyuma.com.glaiddriver.models.live.PendingOrder
 import festusyuma.com.glaiddriver.request.OrderRequests
@@ -59,13 +54,13 @@ class NewOrderFragment : Fragment(R.layout.fragment_new_order) {
                 .format(livePendingOrder.quantity.value, livePendingOrder.gasUnit.value)
 
         gasType.text = livePendingOrder.gasType.value?.capitalizeWords()
-        callCustomerButton.text = livePendingOrder.customer.value?.tel
-
-        callCustomerButton.setOnClickListener { callCustomer(livePendingOrder.customer.value?.tel) }
+        callCustomerButton.setOnClickListener { callCustomer() }
         textCustomerButton.setOnClickListener { chat() }
+        deliverButton.setOnClickListener { deliverOrder() }
     }
 
-    private fun callCustomer(tel: String?) {
+    private fun callCustomer() {
+        val tel = livePendingOrder.customer.value?.tel
         if (tel != null) {
             val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", tel.addCountryCode(), null))
             startActivity(intent)
@@ -77,39 +72,21 @@ class NewOrderFragment : Fragment(R.layout.fragment_new_order) {
         startActivity(intent)
     }
 
-    private fun deliveryTrip() {
-
-    }
-
-    val clickListener = View.OnClickListener { view ->
-        view.startAnimation(buttonClickAnim)
-        when (view.id) {
-            R.id.callCustomerButton -> {
-                //run call intent
-
-            }
-            R.id.textCustomerButton -> {
-
-            }
-            R.id.deliverButton -> {
-                when (deliverButton.text) {
-                    getString(R.string.start_delivery) -> {
-                        /*//Start delivery Api should be initiated here
-                        deliverButton.text = getString(R.string.complete_delivery)
-                        mainOrderMessage.text = getString(R.string.starting_delivery)*/
-
-                        OrderRequests(requireActivity()).startTrip {
-                            Log.v(API_LOG_TAG, it.getString("message"))
-                        }
-                    }
-                    getString(R.string.complete_delivery) -> {
-                        val toast = Toast.makeText(activity, deliverButton.text, Toast.LENGTH_SHORT)
-                        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
-                        toast.show()
-                        //Load the Genereate invoice page from here
-                    }
+    private fun deliverOrder() {
+        when (deliverButton.text) {
+            getString(R.string.start_delivery) -> {
+                OrderRequests(requireActivity()).startTrip {
+                    deliverButton.text = getString(R.string.complete_delivery)
+                    mainOrderMessage.text = getString(R.string.starting_delivery)
                 }
-
+            }
+            getString(R.string.complete_delivery) -> {
+                OrderRequests(requireActivity()).completeTrip {
+                    requireActivity().supportFragmentManager.beginTransaction()
+                        .setCustomAnimations(R.anim.slide_up, R.anim.slide_down, R.anim.slide_up, R.anim.slide_down)
+                        .replace(R.id.frameLayoutId, DashboardFragment())
+                        .commit()
+                }
             }
         }
     }
